eb5efbed6b561bf8294981ca58c02fe2637e90b3
Andrew Rout <32005538+arout77@users.noreply.github.com>
2026-06-26 22:20:03 -0400

    Update HttpException.php

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -6,7 +6,7 @@ class HttpException extends \Exception
     protected int $statusCode;
 
     // Change the default $code to match $statusCode
-    public function __construct(int $statusCode, string $message = "", int $code = null,  ? \Throwable $previous = null)
+    public function __construct(int $statusCode, string $message = "", ?int $code = null,  ? \Throwable $previous = null)
     {
         $this->statusCode = $statusCode;
         // Forward the statusCode to the native parent exception code if no explicit code is passed
